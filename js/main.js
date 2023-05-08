1058b356e4bd6f1ff6ec2af2405ae2ebbba0f275
Nicolas <nicolas_jofre@outlook.com.ar>
2023-05-08 17:58:50 -0300

    Confirmación de elniacion de tarea

--- js/main.js
+++ js/main.js
@@ -24,47 +24,56 @@ form.addEventListener("submit", (event) => {
   // Guardo en almacenamiento local localStorage, para que queden los datos cuando reinicie la página
   localStorage.setItem("tareas", JSON.stringify(tareas));
   renderTareas();
-}); 
-    // Mostramos las tareas de forma dinamica 
-  const renderTareas = () => {
-    tareas = JSON.parse(localStorage.getItem("tareas")) || [];
-    // Capturamos la tabla 
-    const tbody = document.querySelector("tbody");
-    tbody.innerHTML = "";
-    // Recorremos foreach y le agremos las tareas en tbody
-    tareas.forEach(
-      (tarea) =>
-        (tbody.innerHTML += `
+});
+// Mostramos las tareas de forma dinamica
+const renderTareas = () => {
+  tareas = JSON.parse(localStorage.getItem("tareas")) || [];
+  // Capturamos la tabla
+  const tbody = document.querySelector("tbody");
+  tbody.innerHTML = "";
+  // Recorremos foreach y le agremos las tareas en tbody
+  tareas.forEach(
+    (tarea) =>
+      (tbody.innerHTML += `
     <tr>
-        <td>${tarea.text}</td>
+        <td class="${tarea.complete ? "complete" : ""}">${tarea.text}</td>
         <td>
-            <button type="button" data-id="${tarea.id}" class="btn-complete">Completar</button>
+            <button type="button" data-id="${
+              tarea.id
+            }" class="btn-complete">Completar</button>
             <button type="button">Editar</button>
-            <button type="button">Borrar</button>
+            <button type="button" onclick="borrarTarea(${
+              tarea.id
+            })">Borrar</button>
         </td>
     </tr>
 `)
-    )};
-        // Alistamos las tareas cuando cargamos la pagina 
-  renderTareas();
+  );
+};
+// Alistamos las tareas cuando cargamos la pagina
+renderTareas();
 
-  document.addEventListener("click", (e) => {
-    if (e.target.classList.contains("btn-complete")){
-        completarTarea(e.target.dataset.id);
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn-complete")) {
+    completarTarea(e.target.dataset.id);
+  }
+});
+const completarTarea = (id) => {
+  tareas.forEach((tarea) => {
+    if (tarea.id == id) {
+      tarea.complete = !tarea.complete;
     }
   });
-  const completarTarea= (id) => {
-    tareas.forEach((tarea) => {
-        if(tarea.id == id){
-            tarea.complete=!tarea.complete;
-        }
-    });
-    localStorage.setItem("tareas", JSON.stringify(tareas));
+  localStorage.setItem("tareas", JSON.stringify(tareas));
+  renderTareas();
+};
+const borrarTarea = (id) => {
+  if (confirm("Está seguro de eliminar?")) {
+    const filtradas = tareas.filter((tarea) => tarea.id != id);
+    localStorage.setItem("tareas", JSON.stringify(filtradas));
     renderTareas();
-  };
-
-  document.addEventListener('DOMContentLoaded', () => 
-  {
-    renderTareas();
+  }
+};
+document.addEventListener("DOMContentLoaded", () => {
+  renderTareas();
 });
-
