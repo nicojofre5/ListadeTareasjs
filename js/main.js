8469fb33dc8bd550e2aa06316753508d4c28f516
Nicolas <nicolas_jofre@outlook.com.ar>
2023-05-08 17:10:17 -0300

    Comentarios y agregacion de tareas dinamicamente

--- js/main.js
+++ js/main.js
@@ -1,14 +1,46 @@
-let tareas = []; 
+// Creo el array para guardar las tareas
+let tareas = [];
+
+// Capturo el formulario para poder procesar los datos
 const form = document.querySelector("form");
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const inputText = document.querySelector('#text');
-    tareas.push({
-        id: Date.now(),
-        text: inputText.value,
-        complete:false,
-    });
-    event.target.reset();
-   
-    localStorage.setItem('tareas', JSON.stringify(tareas));
+
+// Capturo el input text y agregamos a un array tareas y reinicio el evento para vaciar el input
+form.addEventListener("submit", (event) => {
+  // Prevengo el comportamiento por default d eformulario
+  event.preventDefault();
+
+  const inputText = document.querySelector("#text");
+
+  // Agrego datos a un array
+  tareas.push({
+    id: Date.now(),
+    text: inputText.value,
+    complete: false,
+  });
+
+  // reseteo el input text
+  event.target.reset();
+
+  // Guardo en almacenamiento local localStorage, para que queden los datos cuando reinicie la página
+  localStorage.setItem("tareas", JSON.stringify(tareas));
 });
+  const renderTareas = () => {
+    tareas = JSON.parse(localStorage.getItem("tareas")) || [];
+
+    const tbody = document.querySelector("tbody");
+    tbody.innerHTML = "";
+    tareas.forEach(
+      (tarea) =>
+        (tbody.innerHTML += `
+    <tr>
+        <td>Tarea 1</td>
+        <td>
+            <button type="button">Completar</button>
+            <button type="button">Editar</button>
+            <button type="button">Borrar</button>
+        </td>
+    </tr>
+`)
+    );
+  };
+
